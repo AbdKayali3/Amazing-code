0c1f2607a50920b19c531bb88bf11ba3c0a6788d
Abd Kayali <akayali532@gmail.com>
2023-01-28 15:51:01 +0200

    adding solarized dark and light themes

--- js/themes.js
+++ js/themes.js
@@ -569,4 +569,133 @@ const themes = [
             {name: "[data-language='c'] .function.call, .lang-c .function.call, .language-c .function.call",value: {'color':"#06b6ef",}}, //
         ]
     },
+    {
+        theme:"solarized-dark",
+        index:13,
+        data: [
+
+            // the pre itself (the main thing that will hold your snippit code) aka "The Big Boss"
+            {name: "pre",value: {'overflow':"visible",'z-index':"0",'border-radius':"10px",'display':"inline-block",'width':"auto",'position':"relative",'max-width':"1200px",'min-width':"600px",'padding':"0",'padding-top':"20px",'padding-bottom':"20px",'word-wrap':"break-word",'font-size':"14px",'background':"#002b36",'color':"#839496",}},
+
+            // header and footer to style them in one div   
+            {name: ".in-pre-additions",value: {'background':"#485556",'height':"15px",'display':"block",'position':"relative",'font-size':"10px"}},
+
+            // header section
+            {name: ".pre-header",value: {'border-radius':"10px 10px 0 0",'text-align':"center",'margin-top':"-20px"}},
+            {name: ".pre-header .circles",value: {'position':"absolute",'top':"5px",'width':"5px",'height':"5px",'border-radius':"50%",}},
+            {name: ".pre-header .circle-1",value: {'left':"10px",'background':"red",}},
+            {name: ".pre-header .circle-2",value: {'left':"20px",'background':"orange",}},
+            {name: ".pre-header .circle-3",value: {'left':"30px",'background':"gray",}},
+
+            // footer section
+            {name: ".pre-footer",value: {'border-radius':"0 0 10px 10px",'margin-bottom':"-20px",}},
+            {name: ".pre-footer .language-footer",value: {'position':"absolute",'bottom':"0",'left':"10px",'font-size':"9px"}},
+            {name: ".pre-footer .info-footer",value: {'left':"unset",'right':"10px",}},
+
+            // code section
+            {name: "pre .comment",value: {'color':"#586e75",}}, //
+            {name: "pre .constant",value: {'color':"#839496",}}, //
+            {name: "pre .storage",value: {'color':"#268bd2",}}, //
+            {name: "pre .string, pre .comment.docstring",value: {'color':"#2aa198",}}, //
+            {name: "pre .string.regexp, pre .support.tag.script, pre .support.tag.style",value: {'color':"#2aa198",}}, //
+            {name: "pre .keyword, pre .selector",value: {'color':"#859900",}}, //
+            {name: "pre .entity.function",value: {'color':"#b58900",}}, //
+            {name: "pre .entity.name.function",value: {'color':"#b58900",}}, //
+            {name: "pre .entity.class",value: {'color':"#b58900",}}, //
+            // {name: "pre .entity",value: {'color':"#6d79de",}},
+            {name: "pre .operator",value: {'color':"#859900",}}, //
+            {name: "pre .integer",value: {'color':"#839496",}}, //
+            {name: "pre .variable.global, pre .variable.class, pre .variable.instance",value: {'color':"#839496",}}, //
+
+            {name: "pre .support",value: {'color':"#839496",}},
+            {name: "pre .inherited-class",value: {'font-style':"italic",}},
+            {name: "[data-language='c'] .function.call, .lang-c .function.call, .language-c .function.call",value: {'color':"#b58900",}}, //
+        ]
+    },
+    {
+        theme:"solarized-light",
+        index:14,
+        data: [
+
+            // the pre itself (the main thing that will hold your snippit code) aka "The Big Boss"
+            {name: "pre",value: {'overflow':"visible",'z-index':"0",'border-radius':"10px",'display':"inline-block",'width':"auto",'position':"relative",'max-width':"1200px",'min-width':"600px",'padding':"0",'padding-top':"20px",'padding-bottom':"20px",'word-wrap':"break-word",'font-size':"14px",'background':"#fdf6e3",'color':"#657b83",}},
+
+            // header and footer to style them in one div   
+            {name: ".in-pre-additions",value: {'background':"#37454A",'height':"15px",'display':"block",'position':"relative",'font-size':"10px"}},
+
+            // header section
+            {name: ".pre-header",value: {'border-radius':"10px 10px 0 0",'text-align':"center",'margin-top':"-20px"}},
+            {name: ".pre-header .circles",value: {'position':"absolute",'top':"5px",'width':"5px",'height':"5px",'border-radius':"50%",}},
+            {name: ".pre-header .circle-1",value: {'left':"10px",'background':"red",}},
+            {name: ".pre-header .circle-2",value: {'left':"20px",'background':"orange",}},
+            {name: ".pre-header .circle-3",value: {'left':"30px",'background':"gray",}},
+
+            // footer section
+            {name: ".pre-footer",value: {'border-radius':"0 0 10px 10px",'margin-bottom':"-20px",}},
+            {name: ".pre-footer .language-footer",value: {'position':"absolute",'bottom':"0",'left':"10px",'font-size':"9px"}},
+            {name: ".pre-footer .info-footer",value: {'left':"unset",'right':"10px",}},
+
+            // code section
+            {name: "pre .comment",value: {'color':"#93a1a1",}}, //
+            {name: "pre .constant",value: {'color':"#657b83",}}, //
+            {name: "pre .storage",value: {'color':"#268bd2",}}, //
+            {name: "pre .string, pre .comment.docstring",value: {'color':"#2aa198",}}, //
+            {name: "pre .string.regexp, pre .support.tag.script, pre .support.tag.style",value: {'color':"#2aa198",}}, //
+            {name: "pre .keyword, pre .selector",value: {'color':"#859900",}}, //
+            {name: "pre .entity.function",value: {'color':"#b58900",}}, //
+            {name: "pre .entity.name.function",value: {'color':"#b58900",}}, //
+            {name: "pre .entity.class",value: {'color':"#b58900",}}, //
+            // {name: "pre .entity",value: {'color':"#6d79de",}},
+            {name: "pre .operator",value: {'color':"#859900",}},
+            {name: "pre .integer",value: {'color':"#657b83",}}, //
+            {name: "pre .variable.global, pre .variable.class, pre .variable.instance",value: {'color':"#657b83",}}, //
+
+            {name: "pre .support",value: {'color':"#657b83",}}, //
+            {name: "pre .inherited-class",value: {'font-style':"italic",}},
+            {name: "[data-language='c'] .function.call, .lang-c .function.call, .language-c .function.call",value: {'color':"#b58900",}}, //
+        ]
+    },
+    {
+        theme:"solarized-dark",
+        index:15,
+        data: [
+
+            // the pre itself (the main thing that will hold your snippit code) aka "The Big Boss"
+            {name: "pre",value: {'overflow':"visible",'z-index':"0",'border-radius':"10px",'display':"inline-block",'width':"auto",'position':"relative",'max-width':"1200px",'min-width':"600px",'padding':"0",'padding-top':"20px",'padding-bottom':"20px",'word-wrap':"break-word",'font-size':"14px",'background':"#002b36",'color':"#839496",}},
+
+            // header and footer to style them in one div   
+            {name: ".in-pre-additions",value: {'background':"#707F80",'height':"15px",'display':"block",'position':"relative",'font-size':"10px"}},
+
+            // header section
+            {name: ".pre-header",value: {'border-radius':"10px 10px 0 0",'text-align':"center",'margin-top':"-20px"}},
+            {name: ".pre-header .circles",value: {'position':"absolute",'top':"5px",'width':"5px",'height':"5px",'border-radius':"50%",}},
+            {name: ".pre-header .circle-1",value: {'left':"10px",'background':"red",}},
+            {name: ".pre-header .circle-2",value: {'left':"20px",'background':"orange",}},
+            {name: ".pre-header .circle-3",value: {'left':"30px",'background':"gray",}},
+
+            // footer section
+            {name: ".pre-footer",value: {'border-radius':"0 0 10px 10px",'margin-bottom':"-20px",}},
+            {name: ".pre-footer .language-footer",value: {'position':"absolute",'bottom':"0",'left':"10px",'font-size':"9px"}},
+            {name: ".pre-footer .info-footer",value: {'left':"unset",'right':"10px",}},
+
+            // code section
+            {name: "pre .comment",value: {'color':"#586e75",}}, //
+            {name: "pre .constant",value: {'color':"#839496",}}, //
+            {name: "pre .storage",value: {'color':"#268bd2",}}, //
+            {name: "pre .string, pre .comment.docstring",value: {'color':"#2aa198",}}, //
+            {name: "pre .string.regexp, pre .support.tag.script, pre .support.tag.style",value: {'color':"#2aa198",}}, //
+            {name: "pre .keyword, pre .selector",value: {'color':"#859900",}}, //
+            {name: "pre .entity.function",value: {'color':"#b58900",}}, //
+            {name: "pre .entity.name.function",value: {'color':"#b58900",}}, //
+            {name: "pre .entity.class",value: {'color':"#b58900",}}, //
+            // {name: "pre .entity",value: {'color':"#6d79de",}},
+            {name: "pre .operator",value: {'color':"#859900",}}, //
+            {name: "pre .integer",value: {'color':"#839496",}}, //
+            {name: "pre .variable.global, pre .variable.class, pre .variable.instance",value: {'color':"#839496",}}, //
+
+            {name: "pre .support",value: {'color':"#839496",}},
+            {name: "pre .inherited-class",value: {'font-style':"italic",}},
+            {name: "[data-language='c'] .function.call, .lang-c .function.call, .language-c .function.call",value: {'color':"#b58900",}}, //
+        ]
+    },
 ];
